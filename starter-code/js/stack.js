c837aa54e2ee8461066c9f8fe8d9f8dfeb7b583e
Enrique Garcia <rickyg218@gmail.com>
2018-01-31 23:11:09 -0600

    i did both iration but i couldn't figure out the DOM

---
+++ starter-code/js/stack.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+$("#add-stack").on("click",function(){
+// var pen = $('#stack-div ')
+console.log($('.stack div:first-child').text(".form-control"))
+$('.stack div:nth-child (5)').prop(".form-control")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,38 @@
 function StackDataStructure () {
+    this.stackControl=[]
+    this.MAX_SIZE = 10;
+    this.isEmpty = function(){
+        if(this.stackControl.length === 0){
+            return true;
+        } else{
+            return false;
+        }
+        
+    }
+
+    
+    this.canPush=function(){
+        return this.stackControl.length<this.MAX_SIZE
+    };
+
+    this.push=function(a){
+        if(this.canPush()){
+        this.stackControl.push(a);
+        return this.stackControl
+        }else{
+            return "Stack Overflow"
+        }
+    };
+
+    this.pop=function(){
+        if(this.isEmpty()){
+            return "Stack Underflow"
+        }else{
+            return this.stackControl.pop();
+        }
+    }
+
 }
+
+
+
